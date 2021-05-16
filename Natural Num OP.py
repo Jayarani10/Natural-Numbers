e9cc57a2722cd8f0a8051c13be2cdba0ee43aa4e
Jayarani10 <83535318+Jayarani10@users.noreply.github.com>
2021-05-16 23:16:17 +0530

    Create Natural Num OP.py

---
+++ Natural Num OP.py
@@ -0,0 +1,17 @@
+>>>
+
+============== RESTART: C:/Users/ELCOT/Desktop/natural numbers.py ==============
+
+Enter a positive numbers 10
+
+the sum is 55
+
+>>>
+
+============== RESTART: C:/Users/ELCOT/Desktop/natural numbers.py ==============
+
+Enter a positive numbers 100
+
+the sum is 5050
+
+>>> 
